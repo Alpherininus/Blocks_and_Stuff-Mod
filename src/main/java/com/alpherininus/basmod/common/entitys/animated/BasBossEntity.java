1fd076fb571bf87042d09a400a05f8a46760e1f6
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-18 02:02:06 +0200

    Updating Entitys

--- src/main/java/com/alpherininus/basmod/common/entitys/animated/BasBossEntity.java
+++ src/main/java/com/alpherininus/basmod/common/entitys/animated/BasBossEntity.java
@@ -210,11 +210,6 @@ public class BasBossEntity extends MonsterEntity implements IAnimatable {
     }
 
     @Override
-    public boolean isSpinAttacking() {
-        return true;
-    }
-
-    @Override
     public boolean canAttack(LivingEntity target) {
         return true;
     }
