78d8bf2310b5c8e23ead5a35aef0b775caa3d38a
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-18 12:29:23 +0200

    Updating Entitys

---
+++ src/main/java/com/alpherininus/basmod/common/items/ThrowingAxItem.java
@@ -0,0 +1,149 @@
+package com.alpherininus.basmod.common.items;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MoverType;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.TridentItem;
+import net.minecraft.item.UseAction;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+
+public class ThrowingAxItem extends TridentItem {
+
+    private final ImmutableMultimap<Attribute, AttributeModifier> throwingAx;
+
+    public ThrowingAxItem(Properties builderIn, float attackDamageIn, float attackSpeedIn) {
+
+        super(builderIn);
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", attackDamageIn, AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", attackSpeedIn, AttributeModifier.Operation.ADDITION));
+        this.throwingAx = builder.build();
+    }
+
+    public boolean canPlayerBreakBlockWhileHolding(BlockState state, World worldIn, BlockPos pos, PlayerEntity player) {
+        return !player.isCreative();
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.SPEAR;
+    }
+
+    public int getUseDuration(ItemStack stack) {
+        return 72000;
+    }
+
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
+        if (entityLiving instanceof PlayerEntity) {
+            PlayerEntity playerentity = (PlayerEntity)entityLiving;
+            int i = this.getUseDuration(stack) - timeLeft;
+            if (i >= 10) {
+                int j = EnchantmentHelper.getRiptideModifier(stack);
+                if (j <= 0 || playerentity.isWet()) {
+                    if (!worldIn.isRemote) {
+                        stack.damageItem(1, playerentity, (player) -> {
+                            player.sendBreakAnimation(entityLiving.getActiveHand());
+                        });
+                        if (j == 0) {
+                            TridentEntity tridententity = new TridentEntity(worldIn, playerentity, stack);
+                            tridententity.setDirectionAndMovement(playerentity, playerentity.rotationPitch, playerentity.rotationYaw, 0.0F, 2.5F + (float)j * 0.5F, 1.0F);
+                            if (playerentity.abilities.isCreativeMode) {
+                                tridententity.pickupStatus = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
+                            }
+
+                            worldIn.addEntity(tridententity);
+                            worldIn.playMovingSound((PlayerEntity)null, tridententity, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            if (!playerentity.abilities.isCreativeMode) {
+                                playerentity.inventory.deleteStack(stack);
+                            }
+                        }
+                    }
+
+                    playerentity.addStat(Stats.ITEM_USED.get(this));
+                    if (j > 0) {
+                        float f7 = playerentity.rotationYaw;
+                        float f = playerentity.rotationPitch;
+                        float f1 = -MathHelper.sin(f7 * ((float)Math.PI / 180F)) * MathHelper.cos(f * ((float)Math.PI / 180F));
+                        float f2 = -MathHelper.sin(f * ((float)Math.PI / 180F));
+                        float f3 = MathHelper.cos(f7 * ((float)Math.PI / 180F)) * MathHelper.cos(f * ((float)Math.PI / 180F));
+                        float f4 = MathHelper.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
+                        float f5 = 3.0F * ((1.0F + (float)j) / 4.0F);
+                        f1 = f1 * (f5 / f4);
+                        f2 = f2 * (f5 / f4);
+                        f3 = f3 * (f5 / f4);
+                        playerentity.addVelocity((double)f1, (double)f2, (double)f3);
+                        playerentity.startSpinAttack(20);
+                        if (playerentity.isOnGround()) {
+                            float f6 = 1.1999999F;
+                            playerentity.move(MoverType.SELF, new Vector3d(0.0D, (double)1.1999999F, 0.0D));
+                        }
+
+                        SoundEvent soundevent;
+                        if (j >= 3) {
+                            soundevent = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
+                        } else if (j == 2) {
+                            soundevent = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
+                        } else {
+                            soundevent = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
+                        }
+
+                        worldIn.playMovingSound((PlayerEntity)null, playerentity, soundevent, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    }
+
+                }
+            }
+        }
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        if (itemstack.getDamage() >= itemstack.getMaxDamage() - 1) {
+            return ActionResult.resultFail(itemstack);
+        } else if (EnchantmentHelper.getRiptideModifier(itemstack) > 0 && !playerIn.isWet()) {
+            return ActionResult.resultFail(itemstack);
+        } else {
+            playerIn.setActiveHand(handIn);
+            return ActionResult.resultConsume(itemstack);
+        }
+    }
+
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damageItem(1, attacker, (entity) -> {
+            entity.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+        });
+        return true;
+    }
+
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        if ((double)state.getBlockHardness(worldIn, pos) != 0.0D) {
+            stack.damageItem(2, entityLiving, (entity) -> {
+                entity.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+            });
+        }
+
+        return true;
+    }
+
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+        return equipmentSlot == EquipmentSlotType.MAINHAND ? this.throwingAx : super.getAttributeModifiers(equipmentSlot);
+    }
+
+    public int getItemEnchantability() {
+        return 1;
+    }
+}
--- src/main/java/com/alpherininus/basmod/core/init/ItemInit.java
+++ src/main/java/com/alpherininus/basmod/core/init/ItemInit.java
@@ -74,6 +74,9 @@ public class ItemInit {
     public static final RegistryObject<Item> BASMOD_BOOK_WAPONS = ITEMS.register("basmod_waponbook",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> THROWING_AX_ITEM = ITEMS.register("throw_ax",
+            () -> new ThrowingAxItem(new Item.Properties().group(ModItemGroupWapons.BAS_MOD_WAPONS), 4f, -2f));
+
     public static final RegistryObject<Item> ASUKA_AXT = ITEMS.register("asuka_axt",
             () -> new AsukaAxt(BasmodToolMaterial.A_TOOL_MATERIAL, 3, -2f,
                     new Item.Properties().group(ModItemGroupWapons.BAS_MOD_WAPONS)));
