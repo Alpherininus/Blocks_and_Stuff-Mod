cd01957aec97d7c01b11a8ca394e37623a8cb7b6
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-18 19:50:31 +0200

    Updating Events

---
+++ src/main/java/com/alpherininus/basmod/common/events/BasmodGameEvents.java
@@ -0,0 +1,88 @@
+package com.alpherininus.basmod.common.events;
+
+import com.alpherininus.basmod.Basmod;
+import com.alpherininus.basmod.core.init.ItemInit;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Quaternion;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Basmod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class BasmodGameEvents {
+
+    @SubscribeEvent
+    public static void onRenderGameOverlay(RenderGameOverlayEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        assert mc.player != null;
+
+        ItemStack mainhand = mc.player.getItemStackFromSlot(EquipmentSlotType.MAINHAND);
+
+        boolean modifiExperience = !event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.EXPERIENCE;
+        boolean modifiPotionIcon = !event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.POTION_ICONS;
+
+        if (modifiExperience) {
+            if (!mc.player.abilities.isCreativeMode) {
+
+                //int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
+                //int posYHeight = event.getWindow().getScaledHeight() - 48;
+
+                int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
+                int posYHeight = event.getWindow().getScaledHeight() - 49;
+
+                int textureWidth = 90;
+                int textureHeight = 9;
+
+                int barWidth = 88; // TODO barWidth => refill Manabar, default is 88 -> 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88.
+                int barHeight = 8;
+
+                mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png"));
+
+                // TODO Manabar Outline
+                mc.ingameGUI.blit(new MatrixStack(),posXWidth, posYHeight, 0, 0, textureWidth, textureHeight);
+
+                // TODO Manabar
+                mc.ingameGUI.blit(new MatrixStack(),posXWidth + 1, posYHeight + 1, 0, 10, barWidth, barHeight);
+
+            }
+
+        }
+
+        if (mainhand.getItem() == ItemInit.ANIMATED_MAGICAL_STAFF.get()) {
+            if (modifiExperience) {
+                if (!mc.player.abilities.isCreativeMode) {
+
+                    int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
+                    int posYHeight = event.getWindow().getScaledHeight() - 49;
+
+                    int textureWidth = 90;
+                    int textureHeight = 9;
+
+                    int barWidth = 88; // TODO barWidth => refill Manabar, default is 88 -> 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88.
+                    int barHeight = 8;
+
+                    mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png"));
+
+                    // TODO Manabar Outline
+                    mc.ingameGUI.blit(new MatrixStack(),posXWidth, posYHeight, 0, 0, textureWidth, textureHeight);
+
+                    // TODO Manabar
+                    mc.ingameGUI.blit(new MatrixStack(),posXWidth + 1, posYHeight + 1, 0, 10, barWidth, barHeight);
+
+                }
+
+            }
+        }
+
+
+    }
+
+}
+
