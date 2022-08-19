f37612faa309a010227e54b8a118668b190e9115
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-19 23:47:00 +0200

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/common/blocks/TrashbinBlock.java
+++ src/main/java/com/alpherininus/basmod/common/blocks/TrashbinBlock.java
@@ -1,19 +1,40 @@
 package com.alpherininus.basmod.common.blocks;
 
 import com.alpherininus.basmod.common.tileentitys.TrashbinTileEntity;
-import net.minecraft.block.BarrelBlock;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.piglin.PiglinTasks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.stats.Stats;
+import net.minecraft.tileentity.BarrelTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
-public class TrashbinBlock extends BarrelBlock {
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class TrashbinBlock extends ContainerBlock {
+
+    public static final DirectionProperty PROPERTY_FACING = BlockStateProperties.FACING;
+    public static final BooleanProperty PROPERTY_OPEN = BlockStateProperties.OPEN;
+
     public TrashbinBlock(Properties properties) {
         super(properties);
     }
@@ -32,6 +53,71 @@ public class TrashbinBlock extends BarrelBlock {
 
             return ActionResultType.CONSUME;
         }
+    }
+
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!state.matchesBlock(newState.getBlock())) {
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+            if (tileentity instanceof IInventory) {
+                InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory)tileentity);
+                worldIn.updateComparatorOutputLevel(pos, this);
+            }
+
+            super.onReplaced(state, worldIn, pos, newState, isMoving);
+        }
+    }
+
+    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+        TileEntity tileentity = worldIn.getTileEntity(pos);
+        if (tileentity instanceof TrashbinTileEntity) {
+            ((TrashbinTileEntity)tileentity).trashTick();
+        }
+
+    }
+
+    @Nullable
+    public TileEntity createNewTileEntity(IBlockReader worldIn) {
+        return new TrashbinTileEntity();
+    }
+
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        if (stack.hasDisplayName()) {
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+            if (tileentity instanceof TrashbinTileEntity) {
+                ((TrashbinTileEntity)tileentity).setCustomName(stack.getDisplayName());
+            }
+        }
+
+    }
+
+    //
+
+    public boolean hasComparatorInputOverride(BlockState state) {
+        return true;
+    }
+
+    public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
+        return Container.calcRedstone(worldIn.getTileEntity(pos));
+    }
+
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(PROPERTY_FACING, rot.rotate(state.get(PROPERTY_FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(PROPERTY_FACING)));
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(PROPERTY_FACING, PROPERTY_OPEN);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(PROPERTY_FACING, context.getNearestLookingDirection().getOpposite());
     }
 
 }
--- src/main/java/com/alpherininus/basmod/common/tileentitys/TrashbinTileEntity.java
+++ src/main/java/com/alpherininus/basmod/common/tileentitys/TrashbinTileEntity.java
@@ -1,16 +1,16 @@
 package com.alpherininus.basmod.common.tileentitys;
 
 import com.alpherininus.basmod.Basmod;
-import net.minecraft.block.BarrelBlock;
+import com.alpherininus.basmod.common.blocks.TrashbinBlock;
+import com.alpherininus.basmod.core.init.BlockInit;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ChestTileEntity;
 import net.minecraft.tileentity.LockableLootTileEntity;
@@ -25,7 +25,9 @@ import net.minecraft.util.text.TranslationTextComponent;
 
 public class TrashbinTileEntity extends LockableLootTileEntity {
 
-    private NonNullList<ItemStack> barrelContents = NonNullList.withSize(27, new ItemStack(Items.AIR));
+    private final ItemStack satckEMPTY = ItemStack.EMPTY;
+    private final int inventorySlotSize = 27;
+    private NonNullList<ItemStack> barrelContents = NonNullList.withSize(inventorySlotSize, satckEMPTY);
     private int numPlayersUsing;
 
     private TrashbinTileEntity(TileEntityType<?> barrelType) {
@@ -35,6 +37,7 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
     public TrashbinTileEntity() {
         this(TileEntityType.BARREL);
     }
+
 
     public CompoundNBT write(CompoundNBT compound) {
         super.write(compound);
@@ -46,8 +49,13 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
     }
 
     public void read(BlockState state, CompoundNBT nbt) {
+        Minecraft minecraft = Minecraft.getInstance();
+        for(int j = 0; j < minecraft.player.container.getInventory().size(); ++j) {
+            minecraft.playerController.sendSlotPacket(ItemStack.EMPTY, j);
+        }
+
         super.read(state, nbt);
-        this.barrelContents = NonNullList.withSize(this.getSizeInventory(), new ItemStack(Items.AIR));
+        this.barrelContents = NonNullList.withSize(this.getSizeInventory(), satckEMPTY);
         if (!this.checkLootAndRead(nbt)) {
             ItemStackHelper.loadAllItems(nbt, this.barrelContents);
         }
@@ -63,7 +71,14 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
     }
 
     protected void setItems(NonNullList<ItemStack> itemsIn) {
-        this.barrelContents = itemsIn;
+        Minecraft minecraft = Minecraft.getInstance();
+        // this.minecraft.player.inventory.setItemStack(ItemStack.EMPTY);
+
+        for(int j = 0; j < minecraft.player.container.getInventory().size(); ++j) {
+            minecraft.playerController.sendSlotPacket(ItemStack.EMPTY, j);
+            // this.minecraft.player.inventory.setItemStack(ItemStack.EMPTY);
+        }
+
     }
 
     protected ITextComponent getDefaultName() {
@@ -82,7 +97,7 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
 
             ++this.numPlayersUsing;
             BlockState blockstate = this.getBlockState();
-            boolean flag = blockstate.get(BarrelBlock.PROPERTY_OPEN);
+            boolean flag = blockstate.get(TrashbinBlock.PROPERTY_OPEN);
             if (!flag) {
                 this.playSound(blockstate, SoundEvents.BLOCK_BARREL_OPEN);
                 this.setOpenProperty(blockstate, true);
@@ -97,7 +112,7 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
         this.world.getPendingBlockTicks().scheduleTick(this.getPos(), this.getBlockState().getBlock(), 5);
     }
 
-    public void barrelTick() {
+    public void trashTick() {
         int i = this.pos.getX();
         int j = this.pos.getY();
         int k = this.pos.getZ();
@@ -106,12 +121,12 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
             this.scheduleTick();
         } else {
             BlockState blockstate = this.getBlockState();
-            if (!blockstate.matchesBlock(Blocks.BARREL)) {
+            if (!blockstate.matchesBlock(BlockInit.TRASHBIN.get())) {
                 this.remove();
                 return;
             }
 
-            boolean flag = blockstate.get(BarrelBlock.PROPERTY_OPEN);
+            boolean flag = blockstate.get(TrashbinBlock.PROPERTY_OPEN);
             if (flag) {
                 this.playSound(blockstate, SoundEvents.BLOCK_BARREL_CLOSE);
                 this.setOpenProperty(blockstate, false);
@@ -128,14 +143,15 @@ public class TrashbinTileEntity extends LockableLootTileEntity {
     }
 
     private void setOpenProperty(BlockState state, boolean open) {
-        this.world.setBlockState(this.getPos(), state.with(BarrelBlock.PROPERTY_OPEN, Boolean.valueOf(open)), 3);
+        this.world.setBlockState(this.getPos(), state.with(TrashbinBlock.PROPERTY_OPEN, Boolean.valueOf(open)), 3);
     }
 
     private void playSound(BlockState state, SoundEvent sound) {
-        Vector3i vector3i = state.get(BarrelBlock.PROPERTY_FACING).getDirectionVec();
+        Vector3i vector3i = state.get(TrashbinBlock.PROPERTY_FACING).getDirectionVec();
         double d0 = (double)this.pos.getX() + 0.5D + (double)vector3i.getX() / 2.0D;
         double d1 = (double)this.pos.getY() + 0.5D + (double)vector3i.getY() / 2.0D;
         double d2 = (double)this.pos.getZ() + 0.5D + (double)vector3i.getZ() / 2.0D;
         this.world.playSound(null, d0, d1, d2, sound, SoundCategory.BLOCKS, 0.5F, this.world.rand.nextFloat() * 0.1F + 0.9F);
     }
+
 }
