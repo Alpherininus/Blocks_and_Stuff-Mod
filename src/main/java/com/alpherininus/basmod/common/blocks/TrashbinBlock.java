a1bbcf785bc22347e01c667be7cca41e2401185f
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-20 01:07:53 +0200

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/common/blocks/TrashbinBlock.java
+++ src/main/java/com/alpherininus/basmod/common/blocks/TrashbinBlock.java
@@ -1,129 +1,40 @@
 package com.alpherininus.basmod.common.blocks;
 
-import com.alpherininus.basmod.common.tileentitys.TrashbinTileEntity;
-import net.minecraft.block.*;
+import com.alpherininus.basmod.core.init.BlockInit;
+import com.alpherininus.basmod.core.init.FluidInit;
+import com.alpherininus.basmod.core.init.ItemInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.monster.piglin.PiglinTasks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.stats.Stats;
-import net.minecraft.tileentity.BarrelTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Random;
 
-public class TrashbinBlock extends ContainerBlock {
+import static net.minecraft.state.properties.BlockStateProperties.LIT;
 
-    public static final DirectionProperty PROPERTY_FACING = BlockStateProperties.FACING;
-    public static final BooleanProperty PROPERTY_OPEN = BlockStateProperties.OPEN;
+public class TrashbinBlock extends Block {
 
     public TrashbinBlock(Properties properties) {
         super(properties);
     }
-
-    @Override
-    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        if (worldIn.isRemote) {
-            return ActionResultType.SUCCESS;
-        } else {
-            TileEntity tileentity = worldIn.getTileEntity(pos);
-            if (tileentity instanceof TrashbinTileEntity) {
-                player.openContainer((TrashbinTileEntity)tileentity);
-                player.addStat(Stats.OPEN_BARREL);
-                PiglinTasks.func_234478_a_(player, true);
-            }
-
-            return ActionResultType.CONSUME;
-        }
-    }
-
-    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (!state.matchesBlock(newState.getBlock())) {
-            TileEntity tileentity = worldIn.getTileEntity(pos);
-            if (tileentity instanceof IInventory) {
-                InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory)tileentity);
-                worldIn.updateComparatorOutputLevel(pos, this);
-            }
-
-            super.onReplaced(state, worldIn, pos, newState, isMoving);
-        }
-    }
-
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        TileEntity tileentity = worldIn.getTileEntity(pos);
-        if (tileentity instanceof TrashbinTileEntity) {
-            ((TrashbinTileEntity)tileentity).trashTick();
-        }
-
-    }
-
-    @Nullable
-    public TileEntity createNewTileEntity(IBlockReader worldIn) {
-        return new TrashbinTileEntity();
-    }
-
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
-    }
-
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
-        if (stack.hasDisplayName()) {
-            TileEntity tileentity = worldIn.getTileEntity(pos);
-            if (tileentity instanceof TrashbinTileEntity) {
-                ((TrashbinTileEntity)tileentity).setCustomName(stack.getDisplayName());
-            }
-        }
-
-    }
-
-    public boolean hasComparatorInputOverride(BlockState state) {
-        return true;
-    }
-
-    public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
-        return Container.calcRedstone(worldIn.getTileEntity(pos));
-    }
-
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return state.with(PROPERTY_FACING, rot.rotate(state.get(PROPERTY_FACING)));
-    }
-
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.toRotation(state.get(PROPERTY_FACING)));
-    }
-
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(PROPERTY_FACING, PROPERTY_OPEN);
-    }
-
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(PROPERTY_FACING, context.getNearestLookingDirection().getOpposite());
-    }
-
-    //
 
     @Override
     public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
@@ -135,8 +46,54 @@ public class TrashbinBlock extends ContainerBlock {
         super.addInformation(stack, worldIn, tooltip, flagIn);
     }
 
-    @Override
-    public boolean isAir(BlockState state, IBlockReader world, BlockPos pos) {
-        return false;
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+
+        PlayerEntity playerIn = null;
+        Hand handIn = null;
+        assert false;
+        ItemStack stackIn = playerIn.getHeldItem(handIn);
+        BlockPos blockpos = playerIn.getPosition();
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+
+        boolean mana = blockstate.getBlock() == BlockInit.TRASHBIN.get().getBlock();
+        if (mana) {
+            if (playerIn.shouldHeal()) {
+                playerIn.heal(1.0F);
+            }
+            stackIn.setDamage(stackIn.getDamage() + 1);
+            if (stackIn.getDamage() >= stackIn.getMaxDamage()) stackIn.setCount(0);
+
+        }
     }
+
+    private void deleteItemstack(BlockState blockState, ItemStack stack, PlayerEntity player, World worldIn, BlockPos pos) {
+        ItemStack mainhand = player.getItemStackFromSlot(EquipmentSlotType.MAINHAND);
+        Item getitem = mainhand.getItem();
+
+        boolean test = getitem.isDamaged(mainhand);
+
+        if (test) {
+            stack.setDamage(stack.getDamage() + 1);
+            if (stack.getDamage() >= stack.getMaxDamage()) stack.setCount(0);
+
+        }
+    }
+
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (worldIn.isRemote) {
+
+        } else {
+            activate(state, worldIn, pos);
+        }
+
+        ItemStack itemstack = player.getHeldItem(handIn);
+        return itemstack.getItem() instanceof BlockItem && (new BlockItemUseContext(player, handIn, itemstack, hit)).canPlace() ? ActionResultType.PASS : ActionResultType.SUCCESS;
+    }
+
+    private static void activate(BlockState state, World world, BlockPos pos) {
+        if (!state.get(LIT)) {
+            world.setBlockState(pos, state.with(LIT, Boolean.TRUE), 3);
+        }
+    }
+
 }
