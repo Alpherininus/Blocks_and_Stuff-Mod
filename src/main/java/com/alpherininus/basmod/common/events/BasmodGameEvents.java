44b27b808f8ccd11fd8f5a01b60c6a8924cbb364
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-08-20 19:21:05 +0200

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/common/events/BasmodGameEvents.java
+++ src/main/java/com/alpherininus/basmod/common/events/BasmodGameEvents.java
@@ -21,6 +21,9 @@ import java.io.IOException;
 @Mod.EventBusSubscriber(modid = Basmod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BasmodGameEvents {
 
+    protected static final ResourceLocation MANA_BARS = new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png");
+    protected static final ResourceLocation DIVING_HELMET_OVERLAY = new ResourceLocation(Basmod.MOD_ID, "textures/misc/diving_helmet_overlay.png");
+
     private Minecraft mc;
     private static FontRenderer fontRenderer;
 
@@ -39,38 +42,46 @@ public class BasmodGameEvents {
         boolean ignoreGamemode = !mc.player.abilities.isCreativeMode && !mc.player.isSpectator();
         boolean notIgnoreGamemode = mc.player.abilities.isCreativeMode && !mc.player.isSpectator();
 
+        boolean isNotInWater = !mc.player.isInWater();
+        boolean isInWater = mc.player.isInWater();
+
         boolean modifiExperience = !event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.EXPERIENCE;
         boolean modifiVignette = !event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.VIGNETTE;
         boolean modifiPotionIcon = !event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.POTION_ICONS;
 
-        if (modifiExperience) {
-            if (ignoreGamemode) {
+        if (isNotInWater) {
+            if (modifiExperience) {
+                if (ignoreGamemode) {
 
-                //int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
-                //int posYHeight = event.getWindow().getScaledHeight() - 48;
+                    //int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
+                    //int posYHeight = event.getWindow().getScaledHeight() - 48;
 
-                int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
-                int posYHeight = event.getWindow().getScaledHeight() - 49;
+                    int posXWidth = event.getWindow().getScaledWidth() / 2 + 10;
+                    int posYHeight = event.getWindow().getScaledHeight() - 49;
 
-                int textureWidth = 90;
-                int textureHeight = 9;
+                    int textureWidth = 90;
+                    int textureHeight = 9;
 
-                int barWidth = 69; // TODO barWidth => refill Manabar, default is 88 -> 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88. / ( 22 = 25% / 44 = 50% / 66 = 75% / 88 = 100% )
-                int barHeight = 7;
+                    int barWidth = 69; // TODO barWidth => refill Manabar, default is 88 -> 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88. / ( 22 = 25% / 44 = 50% / 66 = 75% / 88 = 100% )
+                    int barHeight = 7;
 
-                mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png"));
+                    mc.getTextureManager().bindTexture(MANA_BARS);
 
-                // TODO Manabar Outline
-                mc.ingameGUI.blit(new MatrixStack(), posXWidth, posYHeight, 0, 0, textureWidth, textureHeight);
+                    // TODO Manabar Outline
+                    mc.ingameGUI.blit(new MatrixStack(), posXWidth, posYHeight, 0, 0, textureWidth, textureHeight);
 
-                // TODO Manabar
-                mc.ingameGUI.blit(new MatrixStack(), posXWidth + 1, posYHeight + 1, 0, 9, barWidth, barHeight);
+                    // TODO Manabar
+                    mc.ingameGUI.blit(new MatrixStack(), posXWidth + 1, posYHeight + 1, 0, 9, barWidth, barHeight);
+
+                }
 
             }
-
         }
 
+        // TODO Eqip Items
+
         if (mainhand.getItem() == ItemInit.ANIMATED_MAGICAL_STAFF.get()) {
+
             if (modifiExperience) {
                 if (ignoreGamemode) {
 
@@ -83,7 +94,7 @@ public class BasmodGameEvents {
                     int barWidth = 88; // TODO barWidth => refill Manabar, default is 88 -> 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88.
                     int barHeight = 7;
 
-                    mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png"));
+                    mc.getTextureManager().bindTexture(MANA_BARS);
 
                     // TODO Manabar Outline
                     mc.ingameGUI.blit(new MatrixStack(), posXWidth, posYHeight, 0, 18, textureWidth, textureHeight);
@@ -109,7 +120,7 @@ public class BasmodGameEvents {
                     int barWidth = 88; // TODO barWidth => refill Manabar, default is 88 -> 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88.
                     int barHeight = 7;
 
-                    mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/gui/hud/mana_bar.png"));
+                    mc.getTextureManager().bindTexture(MANA_BARS);
 
                     // TODO Manabar Outline
                     mc.ingameGUI.blit(new MatrixStack(), posXWidth, posYHeight, 0, 36, textureWidth, textureHeight);
@@ -120,14 +131,16 @@ public class BasmodGameEvents {
                 }
             }
         }
-        //
 
+        // TODO Diving Helmet
         if (head.getItem() == ItemInit.DIVING_HELMET_HELMET.get()) {
-            if (modifiExperience) {
+            event.getWindow().setWindowTitle("Hello im Diving!");
+
+            if (modifiVignette) {
                 if (notIgnoreGamemode) {
 
-                    int posXWidth = event.getWindow().getScaledWidth() / 2 + 1;
-                    int posYHeight = event.getWindow().getScaledHeight() - 1;
+                    int posXWidth = event.getWindow().getScaledWidth() / 2 + 768;
+                    int posYHeight = event.getWindow().getScaledHeight() + 1024;
 
                     int textureWidth = 265;
                     int textureHeight = 265;
@@ -135,14 +148,15 @@ public class BasmodGameEvents {
                     int barWidth = 250;
                     int barHeight = 250;
 
-                    mc.getTextureManager().bindTexture(new ResourceLocation(Basmod.MOD_ID, "textures/misc/diving_helmet_overlay.png"));
+                    mc.getTextureManager().bindTexture(DIVING_HELMET_OVERLAY);
+                    event.getWindow().setGuiScale(768.0D);
 
                     // TODO Helmet Screen
                     mc.ingameGUI.blit(new MatrixStack(), posXWidth, posYHeight, 0, 0, textureWidth, textureHeight);
 
                     // TODO Closed texture
                     if (mc.player.isInWater()) {
-                        mc.ingameGUI.blit(new MatrixStack(), posXWidth + 3, posYHeight + 3, 0, 265, barWidth, barHeight);
+                        mc.ingameGUI.blit(new MatrixStack(), posXWidth + 3, posYHeight + 3, 0, 0, barWidth, barHeight);
                     }
 
 
@@ -150,6 +164,7 @@ public class BasmodGameEvents {
             }
         }
     }
+
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
