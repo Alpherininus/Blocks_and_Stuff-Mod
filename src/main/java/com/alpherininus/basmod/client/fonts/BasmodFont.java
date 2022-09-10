b4da5449adb6c59b4cc923186ca6761e56407b32
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-09-11 00:19:19 +0200

    Updating Files, Classes and Packages

---
+++ src/main/java/com/alpherininus/basmod/client/fonts/BasmodFont.java
@@ -0,0 +1,4 @@
+package com.alpherininus.basmod.client.fonts;
+
+public class BasmodFont {
+}
--- src/main/java/com/alpherininus/basmod/common/events/ClientEventBusSubcriber.java
+++ src/main/java/com/alpherininus/basmod/common/events/ClientEventBusSubcriber.java
@@ -4,7 +4,6 @@ import com.alpherininus.basmod.Basmod;
 import com.alpherininus.basmod.core.init.KeybindsInit;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
