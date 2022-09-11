be7534dc1bb594a5c61bdc7014737b086a51ebd7
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-09-11 16:03:32 +0200

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/core/init/AttributesInit.java
+++ src/main/java/com/alpherininus/basmod/core/init/AttributesInit.java
@@ -3,6 +3,7 @@ package com.alpherininus.basmod.core.init;
 import com.alpherininus.basmod.Basmod;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraft.util.registry.Registry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,6 +14,12 @@ public class AttributesInit {
 
     public static final RegistryObject<Attribute> MAX_MANA = ATTRIBUTES_DEFERRED_REGISTER.register("magic.max_mana",
             () -> new RangedAttribute("attribute.name.magic.max_mana", 0.0D, 0.0D, 88.0D).setShouldWatch(true));
+
+    //public final Attribute MAX_MANA_A = registerAttribute("generic.max_mana", (new RangedAttribute("attribute.name.generic.max_mana", 20.0D, 1.0D, 1024.0D)).setShouldWatch(true));
+
+    //private Attribute registerAttribute(String id, Attribute attribute) {
+    //    return Registry.register(Registry.ATTRIBUTE, id, attribute);
+    //}
 
     public static void register(IEventBus eventBus) {
         ATTRIBUTES_DEFERRED_REGISTER.register(eventBus);
--- src/main/java/com/alpherininus/basmod/core/init/EffectInit.java
+++ src/main/java/com/alpherininus/basmod/core/init/EffectInit.java
@@ -3,18 +3,24 @@ package com.alpherininus.basmod.core.init;
 import com.alpherininus.basmod.Basmod;
 import com.alpherininus.basmod.common.effekts.FreezeEffect;
 import com.alpherininus.basmod.common.effekts.ManaEffect;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
+import net.minecraft.potion.HealthBoostEffect;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.UUID;
 
 public class EffectInit {
     public static final DeferredRegister<Effect> POTIONS = DeferredRegister.create(ForgeRegistries.POTIONS, Basmod.MOD_ID);
 
     public static final RegistryObject<Effect> FREEZE = POTIONS.register("freeze", () -> new FreezeEffect(EffectType.HARMFUL, 137207240));
 
+    // public static final RegistryObject<Effect> MANA = POTIONS.register("mana", () -> new ManaEffect(EffectType.BENEFICIAL, 1360255).addAttributesModifier(AttributesInit.MAX_MANA.get(), "0502a004-c44a-4af8-866c-283bebdef6de", 8.0D, AttributeModifier.Operation.ADDITION));
     public static final RegistryObject<Effect> MANA = POTIONS.register("mana", () -> new ManaEffect(EffectType.BENEFICIAL, 1360255));
 
 
