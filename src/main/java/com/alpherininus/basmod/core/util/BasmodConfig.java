d812764ef6982c086408ea21a0664355b87b5064
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-09-19 12:26:09 +0200

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/core/util/BasmodConfig.java
+++ src/main/java/com/alpherininus/basmod/core/util/BasmodConfig.java
@@ -29,16 +29,16 @@ public class BasmodConfig {
         CONFIG_BUILDER.comment("general configs").push("Basmod Configs");
 
         // RenderGameOverlayEvent
-        config_integer_manabar = CONFIG_BUILDER.comment("Default value is Maximum: 88.").define("Config Manabar", 88);
-        config_integer_magicalstaff = CONFIG_BUILDER.comment("Default value is Maximum: 88.").define("Config Magical Staff bar", 88);
-        config_integer_healstaff = CONFIG_BUILDER.comment("Default value is Maximum: 88.").define("Config Heal Staff bar", 88);
+        config_integer_manabar = CONFIG_BUILDER.comment("Default value is Bar Maximum: 88.").worldRestart().define("Config Manabar", 88);
+        config_integer_magicalstaff = CONFIG_BUILDER.comment("Default value is Bar Maximum: 88.").worldRestart().define("Config Magical Staff bar", 88);
+        config_integer_healstaff = CONFIG_BUILDER.comment("Default value is Bar Maximum: 88.").worldRestart().define("Config Heal Staff bar", 88);
 
         //
 
         CONFIG_BUILDER.pop();
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        COMMON_SPEC_GENERAL = CONFIG_BUILDER.worldRestart().build();
+        COMMON_SPEC_GENERAL = CONFIG_BUILDER.build();
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -46,8 +46,8 @@ public class BasmodConfig {
         CONFIG_BUILDER.comment("Config your Item").push("Magical Staff Configs");
 
         // Magical Staff
-        config_integer_ad_magical_staff = CONFIG_BUILDER.comment("Default value is 2.").define("Magical Staff Attackdamadge", 2);
-        config_float_as_magical_staff = CONFIG_BUILDER.comment("Default value is -2.0F.").define("Magical Staff Attackspeed", -2.0F);
+        config_integer_ad_magical_staff = CONFIG_BUILDER.comment("Default value is 2.").worldRestart().define("Magical Staff Attackdamadge", 2);
+        config_float_as_magical_staff = CONFIG_BUILDER.comment("Default value is -2.0F.").worldRestart().define("Magical Staff Attackspeed", 0.20F);
         config_thunder_magical_staff = CONFIG_BUILDER.comment("Default value is true.").define("Spawn Thunder", true);
         config_tnt_magical_staff = CONFIG_BUILDER.comment("Default value is false.").define("Spawn TNT", false);
         config_fireball_magical_staff = CONFIG_BUILDER.comment("Default value is false.").define("Spawn Fireball", false);
