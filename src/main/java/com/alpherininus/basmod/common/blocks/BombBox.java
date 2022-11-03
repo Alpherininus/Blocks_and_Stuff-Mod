bebb2bbedd3cbd08cd5bff0dcf12dc016cecae72
Alpherininus <68104742+Alpherininus@users.noreply.github.com>
2022-11-03 21:43:59 +0100

    Updating Files, Classes and Packages

--- src/main/java/com/alpherininus/basmod/common/blocks/BombBox.java
+++ src/main/java/com/alpherininus/basmod/common/blocks/BombBox.java
@@ -1,60 +1,31 @@
 package com.alpherininus.basmod.common.blocks;
 
 import com.alpherininus.basmod.Basmod;
-import com.mojang.brigadier.StringReader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.item.ArmorStandEntity;
-import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particles.BlockParticleData;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Random;
 
 public class BombBox extends Block {
 
     public BombBox(Properties properties) {
         super(properties);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-        float chance = 0.35f;
-        if(chance < rand.nextFloat()) {
-            worldIn.addParticle(ParticleTypes.PORTAL, pos.getX() + rand.nextDouble(),
-                    pos.getY() + 0.5D, pos.getZ() + rand.nextDouble(),
-                    0d,0.05d,0d);
-
-            worldIn.addParticle(new BlockParticleData(ParticleTypes.FALLING_DUST, stateIn), pos.getX() + rand.nextDouble(),
-                    pos.getY() + 0.5D, pos.getZ() + rand.nextDouble(),
-                    0.0D, 0.05D, 0.0D);
-
-            worldIn.addParticle(ParticleTypes.FLAME, pos.getX() + rand.nextDouble(),
-                    pos.getY() + 0.5D, pos.getZ() + rand.nextDouble(),
-                    0d,0.05d,0d);
-
-        }
     }
 
     @Override
@@ -85,27 +56,13 @@ public class BombBox extends Block {
                 IWorld world = event.getWorld();
                 BlockPos pos = event.getPos();
 
-                //if (world instanceof World) {
-                //    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
-                //    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
-                //    world.addEntity(armorStand);
-                //}
-
-                if (player.experienceLevel <= 10 && !player.isCreative()) {
-                    world.playSound(player, pos, SoundEvents.BLOCK_ANVIL_BREAK, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                if (world instanceof World) {
+                    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
+                    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
+                    armorStand.addTag("armor_stand_0");
+                    world.addEntity(armorStand);
                 }
 
-                if (!player.isCreative())
-                    player.giveExperiencePoints(-5);
-
-                world.playSound(player, pos, SoundEvents.AMBIENT_UNDERWATER_LOOP, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                for (int index = 0; index < world.getRandom().nextInt(10) + 7; index++) {
-                    PigEntity pig = new PigEntity(EntityType.PIG, (World) world);
-                    pig.setPosition(pos.getX() + world.getRandom().nextInt(10) - 5,
-                            pos.getY(), pos.getZ() + world.getRandom().nextInt(10) - 5);
-                    pig.addTag("bomb_tier_0");
-                    world.addEntity(pig);
-                }
             }
         }
 
@@ -131,26 +88,10 @@ public class BombBox extends Block {
                 IWorld world = event.getWorld();
                 BlockPos pos = event.getPos();
 
-                //if (world instanceof World) {
-                //    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
-                //    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
-                //    world.addEntity(armorStand);
-                //}
-
-                if (player.experienceLevel <= 10 && !player.isCreative()) {
-                    world.playSound(player, pos, SoundEvents.BLOCK_ANVIL_BREAK, SoundCategory.PLAYERS, 1.0f, 1.0f);
-                }
-
-                if (!player.isCreative())
-                    player.giveExperiencePoints(-5);
-
-                world.playSound(player, pos, SoundEvents.AMBIENT_UNDERWATER_LOOP, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                for (int index = 0; index < world.getRandom().nextInt(10) + 7; index++) {
-                    ArmorStandEntity pig = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
-                    pig.setPosition(pos.getX() + world.getRandom().nextInt(10) - 5,
-                            pos.getY(), pos.getZ() + world.getRandom().nextInt(10) - 5);
-                    pig.addTag("bomb_tier_1");
-                    world.addEntity(pig);
+                if (world instanceof World) {
+                    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
+                    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
+                    world.addEntity(armorStand);
                 }
             }
         }
@@ -177,26 +118,10 @@ public class BombBox extends Block {
                 IWorld world = event.getWorld();
                 BlockPos pos = event.getPos();
 
-                //if (world instanceof World) {
-                //    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
-                //    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
-                //    world.addEntity(armorStand);
-                //}
-
-                if (player.experienceLevel <= 10 && !player.isCreative()) {
-                    world.playSound(player, pos, SoundEvents.BLOCK_ANVIL_BREAK, SoundCategory.PLAYERS, 1.0f, 1.0f);
-                }
-
-                if (!player.isCreative())
-                    player.giveExperiencePoints(-5);
-
-                world.playSound(player, pos, SoundEvents.AMBIENT_UNDERWATER_LOOP, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                for (int index = 0; index < world.getRandom().nextInt(10) + 7; index++) {
-                    LightningBoltEntity pig = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, (World) world);
-                    pig.setPosition(pos.getX() + world.getRandom().nextInt(10) - 5,
-                            pos.getY(), pos.getZ() + world.getRandom().nextInt(10) - 5);
-                    pig.addTag("bomb_tier_2");
-                    world.addEntity(pig);
+                if (world instanceof World) {
+                    ArmorStandEntity armorStand = new ArmorStandEntity(EntityType.ARMOR_STAND, (World) world);
+                    armorStand.setPosition(pos.getX(), pos.getY(), pos.getZ());
+                    world.addEntity(armorStand);
                 }
             }
         }
--- src/main/java/com/alpherininus/basmod/common/entitys/MagicalSpellArrowEntity.java
+++ src/main/java/com/alpherininus/basmod/common/entitys/MagicalSpellArrowEntity.java
@@ -1,5 +1,6 @@
 package com.alpherininus.basmod.common.entitys;
 
+import com.alpherininus.basmod.core.init.BlockInit;
 import com.alpherininus.basmod.core.init.ItemInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
@@ -7,6 +8,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
@@ -37,6 +39,18 @@ public class MagicalSpellArrowEntity extends AbstractArrowEntity {
 
     @Override
     protected void onInsideBlock(BlockState state) {
+        BlockPos pos = new BlockPos(this.getPosX(), this.getPosY(), this.getPosZ());
+
+        if (state.getBlock() == BlockInit.BOMB_BOX_LVL0.get()) {
+            world.destroyBlock(pos, false);
+        } else if (state.getBlock() == BlockInit.BOMB_BOX_LVL1.get()) {
+            world.destroyBlock(pos, false);
+        } else if (state.getBlock() == BlockInit.BOMB_BOX_LVL2.get()) {
+            world.destroyBlock(pos, false);
+        } else {
+            return;
+        }
+
         super.onInsideBlock(state);
     }
 
@@ -47,6 +61,7 @@ public class MagicalSpellArrowEntity extends AbstractArrowEntity {
 
     @Override
     public void tick() {
+
         if (this.timeInGround > 60) {
             this.remove();
         }
